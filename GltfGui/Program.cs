3cc775edb2f4f3f98c04162f3b87c12d2bfc5649
ousttrue <ousttrue@gmail.com>
2020-07-01 07:45:32 +0900

    init

---
+++ GltfGui/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GltfGui
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # GltfGui
+
 gltf viewer
+
